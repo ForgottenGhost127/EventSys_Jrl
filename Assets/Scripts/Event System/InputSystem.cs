97d551b0632cb80aa5bdae5c9757da899d2c3e0c
LAPTOP-0CENV2U2\jufla <judith.legaz@hotmail.com>
2025-01-24 21:15:55 +0100

    no message

--- Assets/Scripts/Event System/InputSystem.cs
+++ Assets/Scripts/Event System/InputSystem.cs
@@ -6,6 +6,7 @@ public class InputSystem : MonoBehaviour
     // Crear un control de teclas desde el que lanzamos los eventos, básicamente, mover aquí cualquier
     // input que controle cualquier evento que sea escuchado por el event system. Este tiene que ser escuchado
     // por el event system. Añade un Empty y guíate por el esquema de Santiago
+    //En la captura falta en el C de Points AddLevel(no se si int o float).
     #region Properties
     #endregion
 
